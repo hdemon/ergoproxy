be327960f2d82afba99bc4d0deef0307694c79dc
Masami Yonehara <zeitdiebe@gmail.com>
2014-05-17 01:52:53 +0900

    refactor

--- app.rb
+++ app.rb
@@ -5,17 +5,47 @@ require 'excon'
 require 'uri'
 # require 'pry'
 
+
+class Request
+  def initialize(env)
+    @env = env
+  end
+
+  def target_uri
+    URI.parse URI.decode params["target_uri"]
+  end
+
+  def target_origin
+    target_uri.scheme +
+      "://" + target_uri.host +
+      (target_uri.port ? ":#{target_uri.port.to_s}" : '')
+  end
+
+  def headers
+    @env.select {|k,v| k.start_with? 'HTTP_'}.each_with_object({}) do |array, hash|
+      hash[array[0].sub(/^HTTP_/, '')] = array[1]
+    end.tap { |hash| hash.delete('HOST') }
+  end
+
+  def params
+    @env["QUERY_STRING"].split('&').each_with_object({}) do |string, object|
+      parts = string.split '='
+      object[parts[0]] = parts[1]
+    end
+  end
+end
+
+
 class App
   def call(env)
-    @env = env
+    request = Request.new env
 
-    uri = URI.parse URI.decode params["target_uri"]
-    conn = Faraday.new(url: origin(uri)) do |faraday|
+    conn = Faraday.new(url: request.target_origin) do |faraday|
       faraday.adapter :excon
     end
 
-    env = conn.get(uri.path) do |req|
-      req.headers = request_headers
+    env = conn.get(request.target_uri.path) do |req|
+      req.headers = request.headers
     end.env
 
     response_array env
@@ -27,24 +57,5 @@ class App
       env.response_headers,
       [env.body],
     ]
-  end
-
-  def origin(uri)
-    uri.scheme +
-      "://" + uri.host +
-      (uri.port ? ":#{uri.port.to_s}" : '')
-  end
-
-  def request_headers
-    @env.select {|k,v| k.start_with? 'HTTP_'}.each_with_object({}) do |array, hash|
-      hash[array[0].sub(/^HTTP_/, '')] = array[1]
-    end.tap { |hash| hash.delete('HOST') }
-  end
-
-  def params
-    @env["QUERY_STRING"].split('&').each_with_object({}) do |string, object|
-      parts = string.split '='
-      object[parts[0]] = parts[1]
-    end
   end
 end
